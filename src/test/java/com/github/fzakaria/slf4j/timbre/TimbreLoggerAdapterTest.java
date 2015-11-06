ba20c82de33812384cd51c08d20e418365d42914
Farid Zakaria <fzakaria@apple.com>
2015-11-06 08:35:17 -0800

    Added tests for every log level

--- src/test/java/com/github/fzakaria/slf4j/timbre/TimbreLoggerAdapterTest.java
+++ src/test/java/com/github/fzakaria/slf4j/timbre/TimbreLoggerAdapterTest.java
@@ -7,7 +7,29 @@ import org.slf4j.LoggerFactory;
 public class TimbreLoggerAdapterTest {
 
     @Test
-    public void basicLogging() {
+    public void basicErrorLogging() {
+        Logger log = LoggerFactory.getLogger("basicLoggin");
+        log.error("Hello World");
+        log.error("Hello World {}", "Farid");
+        log.error("Hello World {} {}", "Farid", "Zakaria");
+        log.error("Hello World {} {} {}", "What", "a", "Beautiful Day!");
+        log.error("Hello World", new Exception("test"));
+        log.error("Hello World {} {}", "Farid", "Zakaria", new Exception("test"));
+    }
+
+    @Test
+    public void basicWarnLogging() {
+        Logger log = LoggerFactory.getLogger("basicLoggin");
+        log.warn("Hello World");
+        log.warn("Hello World {}", "Farid");
+        log.warn("Hello World {} {}", "Farid", "Zakaria");
+        log.warn("Hello World {} {} {}", "What", "a", "Beautiful Day!");
+        log.warn("Hello World", new Exception("test"));
+        log.warn("Hello World {} {}", "Farid", "Zakaria", new Exception("test"));
+    }
+
+    @Test
+    public void basicInfoLogging() {
         Logger log = LoggerFactory.getLogger("basicLoggin");
         log.info("Hello World");
         log.info("Hello World {}", "Farid");
@@ -16,4 +38,27 @@ public class TimbreLoggerAdapterTest {
         log.info("Hello World", new Exception("test"));
         log.info("Hello World {} {}", "Farid", "Zakaria", new Exception("test"));
     }
+
+    @Test
+    public void basicDebugLogging() {
+        Logger log = LoggerFactory.getLogger("basicLoggin");
+        log.debug("Hello World");
+        log.debug("Hello World {}", "Farid");
+        log.debug("Hello World {} {}", "Farid", "Zakaria");
+        log.debug("Hello World {} {} {}", "What", "a", "Beautiful Day!");
+        log.debug("Hello World", new Exception("test"));
+        log.debug("Hello World {} {}", "Farid", "Zakaria", new Exception("test"));
+    }
+
+    @Test
+    public void basicTraceLogging() {
+        Logger log = LoggerFactory.getLogger("basicLoggin");
+        log.trace("Hello World");
+        log.trace("Hello World {}", "Farid");
+        log.trace("Hello World {} {}", "Farid", "Zakaria");
+        log.trace("Hello World {} {} {}", "What", "a", "Beautiful Day!");
+        log.trace("Hello World", new Exception("test"));
+        log.trace("Hello World {} {}", "Farid", "Zakaria", new Exception("test"));
+    }
+    
 }
