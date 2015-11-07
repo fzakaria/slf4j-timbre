84c7c82c9b810b3c34226bac3b38f13eddc1d91b
rufo <rufo@rufoa.com>
2015-11-07 18:36:00 +0000

    factor common methods in adapter using a macro

--- src/clj/slf4j_timbre/adapter.clj
+++ src/clj/slf4j_timbre/adapter.clj
@@ -5,107 +5,33 @@
 	(:require [taoensso.timbre :as timbre])
 	(:import [org.slf4j.helpers FormattingTuple MarkerIgnoringBase MessageFormatter]))
 
-(defn -isErrorEnabled
-	[this]
-	(timbre/log? :error))
+(defmacro ^:private wrap
+	[method]
+	`(fn
+		([_# msg#]
+			(~method msg#))
+		([_# msg# o1# o2#]
+			(let [ft# (MessageFormatter/format msg# o1# o2#)]
+				(~method (.getThrowable ft#) (.getMessage ft#))))
+		([_# msg# o#]
+			(cond
+				(string? o#)
+					(let [ft# (MessageFormatter/format msg# o#)]
+						(~method (.getThrowable ft#) (.getMessage ft#)))
+				(.isArray (class o#))
+					(let [ft# (MessageFormatter/arrayFormat msg# o#)]
+						(~method (.getThrowable ft#) (.getMessage ft#)))
+				(isa? (class o#) Throwable)
+					(~method o# msg#)))))
 
-(defn -error
-	([this msg]
-		(timbre/error msg))
-	([this msg o1 o2]
-		(let [ft (MessageFormatter/format msg o1 o2)]
-			(timbre/error (.getThrowable ft) (.getMessage ft))))
-	([this msg o]
-		(cond
-			(string? o)
-				(let [ft (MessageFormatter/format msg o)]
-					(timbre/error (.getThrowable ft) (.getMessage ft)))
-			(.isArray (class o))
-				(let [ft (MessageFormatter/arrayFormat msg o)]
-					(timbre/error (.getThrowable ft) (.getMessage ft)))
-			(isa? (class o) Throwable)
-				(timbre/error o msg))))
+(def -error (wrap timbre/error))
+(def -warn  (wrap timbre/warn))
+(def -info  (wrap timbre/info))
+(def -debug (wrap timbre/debug))
+(def -trace (wrap timbre/trace))
 
-(defn -isWarnEnabled
-	[this]
-	(timbre/log? :warn))
-
-(defn -warn
-	([this msg]
-		(timbre/warn msg))
-	([this msg o1 o2]
-		(let [ft (MessageFormatter/format msg o1 o2)]
-			(timbre/warn (.getThrowable ft) (.getMessage ft))))
-	([this msg o]
-		(cond
-			(string? o)
-				(let [ft (MessageFormatter/format msg o)]
-					(timbre/warn (.getThrowable ft) (.getMessage ft)))
-			(.isArray (class o))
-				(let [ft (MessageFormatter/arrayFormat msg o)]
-					(timbre/warn (.getThrowable ft) (.getMessage ft)))
-			(isa? (class o) Throwable)
-				(timbre/warn o msg))))
-
-(defn -isInfoEnabled
-	[this]
-	(timbre/log? :info))
-
-(defn -info
-	([this msg]
-		(timbre/info msg))
-	([this msg o1 o2]
-		(let [ft (MessageFormatter/format msg o1 o2)]
-			(timbre/info (.getThrowable ft) (.getMessage ft))))
-	([this msg o]
-		(cond
-			(string? o)
-				(let [ft (MessageFormatter/format msg o)]
-					(timbre/info (.getThrowable ft) (.getMessage ft)))
-			(.isArray (class o))
-				(let [ft (MessageFormatter/arrayFormat msg o)]
-					(timbre/info (.getThrowable ft) (.getMessage ft)))
-			(isa? (class o) Throwable)
-				(timbre/info o msg))))
-
-(defn -isDebugEnabled
-	[this]
-	(timbre/log? :debug))
-
-(defn -debug
-	([this msg]
-		(timbre/debug msg))
-	([this msg o1 o2]
-		(let [ft (MessageFormatter/format msg o1 o2)]
-			(timbre/debug (.getThrowable ft) (.getMessage ft))))
-	([this msg o]
-		(cond
-			(string? o)
-				(let [ft (MessageFormatter/format msg o)]
-					(timbre/debug (.getThrowable ft) (.getMessage ft)))
-			(.isArray (class o))
-				(let [ft (MessageFormatter/arrayFormat msg o)]
-					(timbre/debug (.getThrowable ft) (.getMessage ft)))
-			(isa? (class o) Throwable)
-				(timbre/debug o msg))))
-
-(defn -isTraceEnabled
-	[this]
-	(timbre/log? :trace))
-
-(defn -trace
-	([this msg]
-		(timbre/trace msg))
-	([this msg o1 o2]
-		(let [ft (MessageFormatter/format msg o1 o2)]
-			(timbre/trace (.getThrowable ft) (.getMessage ft))))
-	([this msg o]
-		(cond
-			(string? o)
-				(let [ft (MessageFormatter/format msg o)]
-					(timbre/trace (.getThrowable ft) (.getMessage ft)))
-			(.isArray (class o))
-				(let [ft (MessageFormatter/arrayFormat msg o)]
-					(timbre/trace (.getThrowable ft) (.getMessage ft)))
-			(isa? (class o) Throwable)
-				(timbre/trace o msg))))
+(defn -isErrorEnabled [_] (timbre/log? :error))
+(defn -isWarnEnabled  [_] (timbre/log? :warn))
+(defn -isInfoEnabled  [_] (timbre/log? :info))
+(defn -isDebugEnabled [_] (timbre/log? :debug))
+(defn -isTraceEnabled [_] (timbre/log? :trace))
