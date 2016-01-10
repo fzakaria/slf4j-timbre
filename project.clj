c3ca172c6f06b3795d53847735d2470475ce1553
rufo <rufo@rufoa.com>
2016-01-10 02:59:52 +0000

    Make slf4j-api a dev-dependency only

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.taoensso/timbre "4.2.0"]
-                 [org.slf4j/slf4j-api "1.7.13"]
                  [org.clojure/tools.reader "1.0.0-alpha2"]]
 
   :plugins [[lein-ancient "0.6.5"]]
 
-  :profiles {:dev {:dependencies [[midje "1.8.1"]]
+  :profiles {:dev {:dependencies [[midje "1.8.1"]
+                                  [org.slf4j/slf4j-api "1.7.13"]]
                    :plugins [[lein-midje "3.2"]]}}
 
   :aot :all
