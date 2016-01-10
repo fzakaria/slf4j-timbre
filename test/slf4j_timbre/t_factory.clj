ad5180344271fa21a60913b0eaa0fd5a8f99b6f9
rufo <rufo@rufoa.com>
2016-01-10 04:10:42 +0000

    Threading test for factory method

--- test/slf4j_timbre/t_factory.clj
+++ test/slf4j_timbre/t_factory.clj
@@ -4,10 +4,14 @@
 (let
 	[logger-1a (org.slf4j.LoggerFactory/getLogger "logger-1")
 	 logger-2  (org.slf4j.LoggerFactory/getLogger "logger-2")
-	 logger-1b (org.slf4j.LoggerFactory/getLogger "logger-1")]
+	 logger-1b (org.slf4j.LoggerFactory/getLogger "logger-1")
+	 logger-1c (future (org.slf4j.LoggerFactory/getLogger "logger-1"))]
 
 	(fact "factory returns same instances for same names"
 		(= logger-1a logger-1b) => true)
 
 	(fact "factory returns different instances for different names"
-		(= logger-1a logger-2) => false))
+		(= logger-1a logger-2) => false)
+
+	(fact "factory returns same instances for same names across different threads"
+		(= logger-1a @logger-1c) => true))
