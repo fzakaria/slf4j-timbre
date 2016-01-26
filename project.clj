c004ba58ac3c254b57abe2893bb73d7236e1cb8d
rufo <rufo@rufoa.com>
2016-01-26 00:41:34 +0000

    Bump version and update deps

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject com.fzakaria/slf4j-timbre "0.2.3"
+(defproject com.fzakaria/slf4j-timbre "0.3.0-SNAPSHOT"
   :description "SLF4J binding for Timbre"
   :url "https://github.com/fzakaria/slf4j-timbre"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.taoensso/timbre "4.2.0"]
+                 [com.taoensso/timbre "4.2.1"]
                  [org.clojure/tools.reader "1.0.0-alpha2"]]
 
   :plugins [[lein-ancient "0.6.5"]]
 
-  :profiles {:dev {:dependencies [[midje "1.8.1"]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]
                                   [org.slf4j/slf4j-api "1.7.14"]]
                    :plugins [[lein-midje "3.2"]]}}
 
