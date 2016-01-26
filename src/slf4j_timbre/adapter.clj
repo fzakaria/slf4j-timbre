5982afede754a84d0252f3085dd9ef543f446224
rufo <rufo@rufoa.com>
2016-01-26 01:43:41 +0000

    Implement Logger directly rather than subclassing MarkerIgnoringBase

--- src/slf4j_timbre/adapter.clj
+++ src/slf4j_timbre/adapter.clj
@@ -1,10 +1,10 @@
 (ns slf4j-timbre.adapter
 	(:gen-class
 		:name com.github.fzakaria.slf4j.timbre.TimbreLoggerAdapter
-		:extends org.slf4j.helpers.MarkerIgnoringBase)
+		:implements [org.slf4j.Logger])
 	(:require [taoensso.timbre :as timbre])
 	(:import
-		[org.slf4j.helpers FormattingTuple MarkerIgnoringBase MessageFormatter]
+		[org.slf4j.helpers FormattingTuple MessageFormatter]
 		org.slf4j.Marker))
 
 (defmacro ^:private wrap
@@ -36,8 +36,18 @@
 (def -debug (wrap timbre/debug))
 (def -trace (wrap timbre/trace))
 
-(defn -isErrorEnabled [_] (timbre/log? :error))
-(defn -isWarnEnabled  [_] (timbre/log? :warn))
-(defn -isInfoEnabled  [_] (timbre/log? :info))
-(defn -isDebugEnabled [_] (timbre/log? :debug))
-(defn -isTraceEnabled [_] (timbre/log? :trace))
+(defn -isErrorEnabled
+	([_]   (timbre/log? :error))
+	([_ _] (timbre/log? :error)))
+(defn -isWarnEnabled
+	([_]   (timbre/log? :warn))
+	([_ _] (timbre/log? :warn)))
+(defn -isInfoEnabled
+	([_]   (timbre/log? :info))
+	([_ _] (timbre/log? :info)))
+(defn -isDebugEnabled
+	([_]   (timbre/log? :debug))
+	([_ _] (timbre/log? :debug)))
+(defn -isTraceEnabled
+	([_]   (timbre/log? :trace))
+	([_ _] (timbre/log? :trace)))
