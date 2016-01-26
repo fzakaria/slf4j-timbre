f919e244c3395f547841e8aa9c753290dfeab09f
rufo <rufo@rufoa.com>
2016-01-26 01:54:42 +0000

    Implement .getName() in logger

--- src/slf4j_timbre/adapter.clj
+++ src/slf4j_timbre/adapter.clj
@@ -1,11 +1,22 @@
 (ns slf4j-timbre.adapter
 	(:gen-class
-		:name com.github.fzakaria.slf4j.timbre.TimbreLoggerAdapter
-		:implements [org.slf4j.Logger])
+		:name         com.github.fzakaria.slf4j.timbre.TimbreLoggerAdapter
+		:implements   [org.slf4j.Logger]
+		:state        state
+		:init         init
+		:constructors {[String] []})
 	(:require [taoensso.timbre :as timbre])
 	(:import
 		[org.slf4j.helpers FormattingTuple MessageFormatter]
 		org.slf4j.Marker))
+
+(defn -init
+	[logger-name]
+	[[] logger-name])
+
+(defn -getName
+	[this]
+	(.state this))
 
 (defmacro ^:private wrap
 	[method]
