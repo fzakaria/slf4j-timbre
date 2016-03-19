1e4e7b86ff1b818fae3c67be8d7f946072de9e52
rufo <rufo@rufoa.com>
2016-03-19 17:17:14 +0000

    Bump timbre dep to 4.3.1 from 4.3.0-RC1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 		 :url "http://www.eclipse.org/legal/epl-v10.html"}
 	:dependencies
 		[[org.clojure/clojure "1.7.0"]
-		 [com.taoensso/timbre "4.3.0-RC1"]
+		 [com.taoensso/timbre "4.3.1"]
 		 [org.slf4j/slf4j-api "1.7.14"]]
 	:profiles
 		{:dev
