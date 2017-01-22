7b6a253a4c98d17b788a0a81badd96f271aa82d7
rufo <rufo@rufoa.com>
2017-01-22 22:39:41 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.fzakaria/slf4j-timbre "0.3.2"
+(defproject com.fzakaria/slf4j-timbre "0.3.3"
 	:description "SLF4J binding for Timbre"
 	:url "https://github.com/fzakaria/slf4j-timbre"
 	:license
