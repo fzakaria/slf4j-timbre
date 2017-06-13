5e90c61272cbce4f1c30d51d321302d2be320e1e
rufo <rufo@rufoa.com>
2017-06-14 00:51:50 +0100

    Version 0.3.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.fzakaria/slf4j-timbre "0.3.6"
+(defproject com.fzakaria/slf4j-timbre "0.3.7-SNAPSHOT"
 	:description "SLF4J binding for Timbre"
 	:url "https://github.com/fzakaria/slf4j-timbre"
 	:license
