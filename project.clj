dbc30ccdc819f977cc957200fae11981068e7b82
rufo <rufus@rufus.al>
2018-07-05 17:43:12 +0100

    Version 0.3.13-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.fzakaria/slf4j-timbre "0.3.12"
+(defproject com.fzakaria/slf4j-timbre "0.3.13-SNAPSHOT"
 	:description "SLF4J binding for Timbre"
 	:url "https://github.com/fzakaria/slf4j-timbre"
 	:license
