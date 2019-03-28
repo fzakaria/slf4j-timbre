682c40dd3a5c4f0730fc5e9b4a199691d34209ef
rufo <rufus@rufus.al>
2019-03-28 02:41:58 +0000

    Bump deps

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
 		{:name "Eclipse Public License"
 		 :url "http://www.eclipse.org/legal/epl-v10.html"}
 	:dependencies
-		[[org.clojure/clojure "1.9.0"]
+		[[org.clojure/clojure "1.10.0"]
 		 [com.taoensso/timbre "4.10.0"]
-		 [org.slf4j/slf4j-api "1.7.25"]]
+		 [org.slf4j/slf4j-api "1.7.26"]]
 	:profiles
 		{:dev
-			{:dependencies [[midje "1.9.1"]]
+			{:dependencies [[midje "1.9.6"]]
 			 :plugins [[lein-midje "3.2.1"]]}}
 	:aot
 		[slf4j-timbre.adapter
