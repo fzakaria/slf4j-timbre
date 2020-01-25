e6f9167421956d9e7eaf6562628fd75eadeddaa7
rufo <rufus@rufus.al>
2020-01-25 22:55:30 +0000

    Version 0.3.18

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.fzakaria/slf4j-timbre "0.3.18-SNAPSHOT"
+(defproject com.fzakaria/slf4j-timbre "0.3.18"
 	:description "SLF4J binding for Timbre"
 	:url "https://github.com/fzakaria/slf4j-timbre"
 	:license
