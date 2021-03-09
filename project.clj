a1bd8e9e857f676a4f6e76dbcfb9b13e982a82bd
rufo <rufus@rufus.al>
2021-03-09 23:44:15 +0000

    Bump deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/fzakaria/slf4j-timbre"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.taoensso/timbre "4.10.0"]
                  [org.slf4j/slf4j-api "1.7.30"]]
   :profiles {:dev
-             {:dependencies [[midje "1.9.9"]]
+             {:dependencies [[midje "1.9.10"]]
               :plugins [[lein-midje "3.2.2"]
                         [lein-sub "0.3.0"]
                         [day8/lein-git-inject "0.0.14"]]
