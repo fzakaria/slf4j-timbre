1967f0d4cc8e5b5174fff8178e87050a61d7c67c
rufo <rufus@rufus.al>
2023-04-04 01:09:45 +0100

    timbre 6

--- integration_tests/timbre6/project.clj
+++ integration_tests/timbre6/project.clj
@@ -1,6 +1,6 @@
 (defproject example "1.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.taoensso/timbre "5.2.1"]
+                 [com.taoensso/timbre "6.1.0"]
                  [org.slf4j/slf4j-api "1.7.36"]
                  ; slf4j-timbre will be added here by lein update-in
                  ]
